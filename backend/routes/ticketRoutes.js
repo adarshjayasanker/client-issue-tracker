a0ba1cbb6b6f0821b6fad125232f8069ca319074
Adarsh Jayasanker <adarshcodes1@gmail.com>
2026-02-24 14:44:32 +0530

    Created authorization middleware to control the permissions and implement it into the endpoints

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -3,12 +3,13 @@ import ticketControllers from '../controllers/ticketControllers.js';
 import verifyToken from '../middlewares/authMiddleware.js';
 import createTicketLimit from '../middlewares/rateLimiter.js';
 import ticketLimiter from '../middlewares/userTicketLimiter.js';
+import authorize from '../middlewares/authorize.js';
 
 const ticketRoute = express.Router();
 
 const {raiseTicket, getTickets} = ticketControllers;
 
-ticketRoute.post('/raiseticket', verifyToken, ticketLimiter, raiseTicket);
+ticketRoute.post('/raiseticket', verifyToken, authorize("user"), ticketLimiter, raiseTicket);
 
 ticketRoute.get('/gettickets', verifyToken, getTickets);
 
